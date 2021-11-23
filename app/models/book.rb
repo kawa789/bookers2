69db1bdf39849f98eaac41598720a611ce95fa8a
EC2 Default User <ec2-user@ip-172-31-92-166.ec2.internal>
2021-11-23 09:39:14 +0000

    [Add]編集機能

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,9 @@
 class Book < ApplicationRecord
   belongs_to :user
   attachment :book
+  attachment :profile_image
+  
+  validates :title, presence: true
+  validates :body, presence: true
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
-  resources :books, only: [:new, :create, :index, :show, :destroy]
-  
+  resources :books, only: [:new, :create, :index, :show, :destroy] do
+  end
+  resources :users, only: [:show, :edit, :update]
 end
